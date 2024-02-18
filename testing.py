84fe83c660d2a8c539c82f958ff4fa8f2ae6079d
Cecegyamfi <141175857+Cecegyamfi@users.noreply.github.com>
2024-02-17 21:49:31 -0500

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,7 @@
+echo "# test_copilot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Cecegyamfi/test_copilot.git
+git push -u origin main
